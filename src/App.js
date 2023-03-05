cf4de86f86093f59b400d56d5036b38ef149a51f
Michael Bunte <michaelwbunte@gmail.com>
2023-03-05 10:30:38 -0800

    added basic homepage and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
+import Homepage from './pages/Homepage';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div>
+            <Routes>
+                <Route exact path="/" element={<Homepage />}/>
+            </Routes>
+        </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Homepage.js
@@ -0,0 +1,10 @@
+function Homepage() {
+//   document.body.style = 'background: black;';
+  return (
+    <div>
+        this is the homepage
+    </div>
+  );
+}
+
+export default Homepage;
