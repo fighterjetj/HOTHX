6d81f3a566fa1582a14fc3fb5d782bf21df6ed8c
Michael Bunte <michaelwbunte@gmail.com>
2023-03-05 16:39:36 -0800

    images can now be uploaded

--- src/components/SparseButton.js
+++ src/components/SparseButton.js
@@ -1,17 +1,21 @@
 import './SparseButton.css'
 
 function SparseButton(props) {
-  let {width, height, type} = props
+  let {width, height, type, heavy, input} = props
 
+  let boxCss = heavy ? "projectBox solid" : "projectBox dashed";
+
+  
+  
   let inner = type == "add" ? (<div className='plusHolder'> + </div>)
-  :  
-  (
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#707070" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-upload"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" y1="3" x2="12" y2="15"></line></svg>
-  )
+  :  ( <div>default</div> );
+
+  inner = input ? ( <div className='input'>{input}</div> ) : inner
+
   
   return (
     <div>
-      <div className="projectBox" style={{width: width, height: height}}>
+      <div className={boxCss} style={{width: width, height: height}}>
         <div className="addProject">
           {inner}
         </div>
--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -1,13 +1,36 @@
 import SparseButton from '../components/SparseButton'
 import '../components/general.css'
 import './uploadpage.css'
+import React, { useState } from 'react';
 
 function UploadPage() {
+    let [uploadedImgs, setUploadedImgs] = useState([]);
+
+    function onFileUpload(e) {
+        setUploadedImgs( (last) =>
+            {return [...last, e.target.files[0]]}
+        );
+        console.log(uploadedImgs)
+        
+    }
+
+    let previews = uploadedImgs.map((uploadImg) => {
+        return(
+            <div>
+                {uploadImg.name}
+            </div>
+        )
+    })
+
     return (
-        <div>
+    <div>
             <h1 className='homepageTitle'>Upload Images</h1>
+            {previews}
             <div className="uploadHolder">
-                <SparseButton width="200px" height="50px" type="upload"/>
+                <input 
+                    type="file" name="file" onChange={onFileUpload}
+                />
+                {/* <button style={{backgroundColor: "white", borderWidth: "0px"}}> */}
             </div>
         </div>
     )
